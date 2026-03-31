66c66358e1455a431db1a03fbd5dd8bf0fb33e05
aycaize <ayca.guler@std.bogazici.edu.tr>
2026-03-31 17:29:56 +0300

    Delete transform directory

--- transform/models/marts/dim_price_type.sql
+++
@@ -1,9 +0,0 @@
-with price_types as (
-    select * from (values
-        (1, 'PTF', 'Piyasa Takas Fiyatı', 'Arz ve talebin kesiştiği saatlik denge fiyatı'),
-        (2, 'SMF', 'Sistem Marginal Fiyatı', 'Dengesizlik durumunda oluşan fiyat'),
-        (3, 'PTF-SMF', 'Fiyat Farkı', 'PTF ile SMF arasındaki fark')
-    ) as t(price_type_id, price_type_code, price_type_name, description)
-)
-
-select * from price_types
